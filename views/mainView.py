2ef8a0877bf28c8c9410a868005b3874c6de1ca5
hotannhan <nhan6662001@gmail.com>
2023-02-28 17:02:26 +0700

    realtime detection YOLOv5

--- views/mainView.py
+++ views/mainView.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.uic import loadUi
+from time import time
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QGraphicsWidget
@@ -9,29 +10,18 @@ from PyQt5.QtWidgets import *
 from views.robotCtrlUi import robotCtrlUi
 
 print("START")
+from PyQt5 import QtGui
 from PyQt5.QtGui import QImage, QPixmap
 
 import numpy as np
 import os
 
-from tflite_model_maker.config import ExportFormat
-from tflite_model_maker import model_spec
-from tflite_model_maker import object_detector
-
-import tensorflow as tf
-assert tf.__version__.startswith('2')
-print(tf. __version__)
-
-#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
-tf.get_logger().setLevel('ERROR')
-from absl import logging
-logging.set_verbosity(logging.ERROR)
-
-spec = model_spec.get('efficientdet_lite2')
-
 import cv2
 from PIL import Image
+
+import torch
+
+
 
 #class View(QWidget):
 class View(QMainWindow):
@@ -45,185 +35,186 @@ class View(QMainWindow):
         self._controller = controller
         self._robotCtrlUi = robotCtrlUi(self._ui)
 
-        os.chdir('E:\HCMUT\FinalProject\GUI\GUI_Arm_Controller\model')
-        interpreter = tf.lite.Interpreter(model_path="EfficientDet2_Flags.tflite")
-        interpreter.allocate_tensors()
-        input_details = interpreter.get_input_details()
-        output_details = interpreter.get_output_details()
-
-        model_path = 'EfficientDet2_Flags.tflite'
-
-        self.classes = ['Vietnam', 'USA', 'Germany', 'Belgium']
-
-        # Define a list of colors for visualization
-        self.COLORS = np.random.randint(0, 255, size=(len(self.classes), 3), dtype=np.uint8)
-
-        self.timer = QTimer()
-        self.timer.timeout.connect(lambda: self.start_detect())
+        # self.model = torch.hub.load('ultralytics/yolov5', 'custom',
+        #                        path='E:\HCMUT\FinalProject\GUI\GUI_Arm_Controller\model\YOLOv5_FlagsDetection.pt')
+        # self.classes = self.model.names  # name of objects
+        # self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         # self.connectBtn.clicked.connect(lambda: self._controller.usbConnect(int(self._ui.vidTxt.toPlainText()),
         #                                                                     int(self._ui.pidTxt.toPlainText())))
         self.connectBtn.clicked.connect(lambda: self._controller.usbConnect(1155, 22336))
-        self.DetectButton.clicked.connect(lambda: self.startTimer())
-        self.StopButton.clicked.connect(lambda: self.endTimer())
+
+        self.DetectButton.clicked.connect(self.start_capture_video)
+        self.StopButton.clicked.connect(self.stop_capture_video)
+        self.thread = {}
 
         self._ui.movCmdBtn.clicked.connect(self.getPosistion)
 
     def getPosistion(self):
         # if str(self._ui.x_txt.toPlainText()) != "" and str(self._ui.y_txt.toPlainText()) != "" and str(self._ui.z_txt.toPlainText()) != "":
-            self._controller.moveCmd(float(self._ui.x_txt.toPlainText()),
-                                     float(self._ui.y_txt.toPlainText()),
-                                     float(self._ui.z_txt.toPlainText()))
-        # else:
-        #     print("At least one field of coordinate posistion is empty")
+        self._controller.moveCmd(float(self._ui.x_txt.toPlainText()),
+                                 float(self._ui.y_txt.toPlainText()),
+                                 float(self._ui.z_txt.toPlainText()))
 
-    def preprocess_image(self, image_path, input_size):
-        """Preprocess the input image to feed to the TFLite model"""
-        img = tf.io.read_file(image_path)
-        img = tf.io.decode_image(img, channels = 3)
-        img = tf.image.convert_image_dtype(img, tf.uint8)
-        original_image = img
-        resized_img = tf.image.resize(img, input_size)
-        resized_img = resized_img[tf.newaxis, :]
-        return resized_img, original_image
+    # else:
+    #     print("At least one field of coordinate posistion is empty")
 
-    def set_input_tensor(self, interpreter, image):
-        """Set the input tensor."""
-        tensor_index = interpreter.get_input_details()[0]['index']
-        input_tensor = interpreter.tensor(tensor_index)()[0]
-        input_tensor[:, :] = image
+    def closeEvent(self, event):
+        self.stop_capture_video()
 
-    def get_output_tensor(self, interpreter, index):
-        """Retur the output tensor at the given index."""
-        output_details = interpreter.get_output_details()[index]
-        tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
-        return tensor
+    def closeEvent(self, event):
+        self.stop_capture_video()
 
-    def detect_objects(self, interpreter, image, threshold):
-        """Returns a list of detection results, each a dictionary of object info."""
-        # Feed the input image to the model
-        self.set_input_tensor(interpreter, image)
-        interpreter.invoke()
+    def stop_capture_video(self):
+        self.thread[1].pause_stream()
+        self.thread[1].stop()
 
-        # Get all outputs from the model
-        scores = self.get_output_tensor(interpreter, 0)
-        boxes = self.get_output_tensor(interpreter, 1)
-        count = int(self.get_output_tensor(interpreter, 2))
-        classes = self.get_output_tensor(interpreter, 3)
+    def start_capture_video(self):
+        self.thread[1] = live_stream(index=1)
+        self.thread[1].start()
 
-        results = []
-        for i in range(count):
-            if scores[i] >= threshold:
-                result = {
-                    'bounding_box': boxes[i],
-                    'class_id': classes[i],
-                    'score': scores[i]
-                }
-                results.append(result)
-        return results
+        self.thread[1].signal.connect(self.show_wedcam)
 
-    def run_odt_and_draw_results(self, image_path, interpreter, threshold=0.5):
-        """Run object detection on the input image and draw the detection results"""
-        # Load the input shape required by the model
-        _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
-
-        # Load the input image and preprocess it
-        preprocessed_image, original_image = self.preprocess_image(
-            image_path,
-            (input_height, input_width)
-        )
-
-        # Run object detection on the input image
-        results = self.detect_objects(interpreter, preprocessed_image, threshold=threshold)
-        print(results)
-        # Plot the detection results on the input image
-        original_image_np = original_image.numpy().astype(np.uint8)
-        for obj in results:
-            # skip noise bounding box
-            if obj['score'] < 0.8:
-                continue
-            # Convert the object bounding box from relative coordinates to absolute
-            # coordinates based on the original image resolution
-            ymin, xmin, ymax, xmax = obj['bounding_box']
-            xmin = int(xmin * original_image_np.shape[1])
-            xmax = int(xmax * original_image_np.shape[1])
-            ymin = int(ymin * original_image_np.shape[0])
-            ymax = int(ymax * original_image_np.shape[0])
-
-            # Find the class index of the current object
-            class_id = int(obj['class_id'])
-
-            # Draw the bounding box and label on the image
-            color = [int(c) for c in self.COLORS[class_id]]
-            cv2.rectangle(original_image_np, (xmin, ymin), (xmax, ymax), color, 2)
-
-            # Draw the center of bounding box
-            center = (int(xmin + (xmax - xmin) // 2), int(ymin + (ymax - ymin) // 2))
-            print(class_id, center)
-            radius = 2
-            cv2.circle(original_image_np, center, radius, color, 2)
-
-            # Make adjustments to make the label visible for all objects
-            y = ymin - 15 if ymin - 15 > 15 else ymin + 15
-            label = "{}: {:.0f}%".format(self.classes[class_id], obj['score'] * 100)
-            cv2.putText(original_image_np, label, (xmin, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-        # Return the final image
-        original_uint8 = original_image_np.astype(np.uint8)
-        return original_uint8
-
-    def photo_capture(self):
-        vid = cv2.VideoCapture(1)  # 0 for laptop webcam, 1 for logi webcam
-        ret, frame = vid.read()
-        print(ret)
-        print(frame)
-        cv2.imwrite('webcam.jpg', frame)
-        # vid.release()
-
-        # Run object detection and show the detection results
-
-        # define a video capture object
-        # vid = cv2.VideoCapture(0)
-
-    def startTimer(self):
-        self.timer.start(1000)
-
-    def endTimer(self):
-        self.timer.stop()
-
-    def start_detect(self):
-
-        interpreter = tf.lite.Interpreter(model_path='EfficientDet2_Flags.tflite')
-        interpreter.allocate_tensors()
-
-        INPUT_IMAGE_URL = "E:\HCMUT\FinalProject\GUI\GUI_Arm_Controller\model\webcam.jpg"
-        DETECTION_THRESHOLD = 0.5
-
-        os.chdir('E:\HCMUT\FinalProject\GUI\GUI_Arm_Controller\model')
-        self.photo_capture()
-
-        # INPUT_IMAGE_URL = "E:/HCMUT/FinalProject/FinalProject/webcamphoto/webcam.jpg"
-        # DETECTION_THRESHOLD = 0.5
-
-        # Load the TFLite model
-        # %cd /content/gdrive/MyDrive/EfficientNet/trainedmodel
-        # interpreter = tf.lite.Interpreter(model_path='EfficientDet2_Flags.tflite')
-        # interpreter.allocate_tensors()
-
-        # Run inference and draw detection result on the local copy of the original file
-        detection_result_image = self.run_odt_and_draw_results(
-            INPUT_IMAGE_URL,
-            interpreter,
-            threshold=DETECTION_THRESHOLD
-        )
-        # imgresized = cv2.resize(detection_result_image, (1000, 1000))
-        # imgresized = cv2.cvtColor(imgresized, cv2.COLOR_RGB2BGR)
-        # cv2.imshow('', imgresized)
-        im = cv2.cvtColor(detection_result_image, cv2.COLOR_RGB2BGR)
-        cv2.imwrite('result.jpg', im)
-
-        self.imgLabel.setPixmap(QPixmap('result.jpg'))
+    def show_wedcam(self, cv_img):
+        """Updates the image_label with a new opencv image"""
+        qt_img = convert_cv_qt(cv_img)
+        self._ui.imgLabel.setPixmap(qt_img)
         self.imgLabel.setScaledContents(True)
+
+def convert_cv_qt(cv_img):
+    """Convert from an opencv image to QPixmap"""
+    rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+    h, w, ch = rgb_image.shape
+    bytes_per_line = ch * w
+    convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+    p = convert_to_Qt_format.scaled(700, 500, Qt.KeepAspectRatio)
+    return QPixmap.fromImage(p)
+
+class live_stream(QThread):
+    signal = pyqtSignal(np.ndarray)
+
+    def __init__(self, index):
+        self.device = None
+        self.out_file = None
+        self.classes = None
+        self.model = None
+        self.gg = True
+        self.player = None
+        self.index = index
+        print("start threading", self.index)
+        super(live_stream, self).__init__()
+
+    def run(self):
+        """
+        Initializes the class with youtube url and output file.
+        :param url: Has to be as youtube URL,on which prediction is made.
+        :param out_file: A valid output file name.
+        """
+        self.model = self.load_model()  # load model
+        self.classes = self.model.names
+        self.out_file = "Labeled_Video.avi"
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.run_program()
+
+    def get_video_from_url(self):
+        """
+        Creates a new video streaming object to extract video frame by frame to make prediction on.
+        :return: opencv2 video capture object, with lowest quality frame available for video.
+        """
+        return cv2.VideoCapture(0)  # "D:/8.Record video/movie.mp4"
+
+    def load_model(self):
+        """
+        Loads Yolo5 model from pytorch hub.
+        :return: Trained Pytorch model.
+        """
+        # model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        model = torch.hub.load('ultralytics/yolov5', 'custom', path='E:\HCMUT\FinalProject\GUI\GUI_Arm_Controller\model\YOLOv5_FlagsDetection')
+        # model = torch.hub.load('yolov5-master', 'custom', path='yolov5s.pt', source='local')
+        return model
+
+    def score_frame(self, frame):
+        """
+        Takes a single frame as input, and scores the frame using yolo5 model.
+        :param frame: input frame in numpy/list/tuple format.
+        :return: Labels and Coordinates of objects detected by model in the frame.
+        """
+        self.model.to(self.device)
+        frame = [frame]
+        results = self.model(frame)
+        labels, cord = results.xyxyn[0][:, -1].numpy(), results.xyxyn[0][:, :-1].numpy()
+        return labels, cord
+
+    def class_to_label(self, x):
+        """
+        For a given label value, return corresponding string label.
+        :param x: numeric label
+        :return: corresponding string label
+        """
+        return self.classes[int(x)]
+
+    def plot_boxes(self, results, frame):
+        """
+        Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
+        :param results: contains labels and coordinates predicted by model on the given frame.
+        :param frame: Frame which has been scored.
+        :return: Frame with bounding boxes and labels ploted on it.
+        """
+        labels, cord = results
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            print("ddd", round(cord[i][4], 2))
+            if row[4] >= 0.1:
+                x1, y1, x2, y2 = int(row[0] * x_shape), int(row[1] * y_shape), int(row[2] * x_shape), int(
+                    row[3] * y_shape)
+                bgr = (0, 255, 0)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]) + " " + str(round(row[4], 2)), (x1, y1),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+
+        return frame
+
+    def run_program(self):
+        """
+        This function is called when class is executed, it runs the loop to read the video frame by frame,
+        and write the output into a new file.
+        :return: void
+        """
+        self.player = self.get_video_from_url()
+        assert self.player.isOpened()
+        x_shape = int(self.player.get(cv2.CAP_PROP_FRAME_WIDTH))
+        y_shape = int(self.player.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        four_cc = cv2.VideoWriter_fourcc(*"MJPG")
+        out = cv2.VideoWriter(self.out_file, four_cc, 20, (x_shape, y_shape))
+
+        while True:
+            os.chdir('E:\HCMUT\FinalProject\GUI\GUI_Arm_Controller\model')
+            start_time = time()
+            ret, frame = self.player.read()
+            assert ret
+            results = self.score_frame(frame)
+            frame = self.plot_boxes(results, frame)
+            cv2.imwrite('result.jpg', frame)
+            end_time = time()
+            fps = 1 / (np.round(end_time - start_time, 3))
+            print(f"Frames Per Second : {round(fps, 2)} FPS")
+            # out.write(frame)
+            self.signal.emit(frame)
+            if not self.gg:
+                print("stop capture video")
+                break
+
+    def stop(self):
+        print("stop threading", self.index)
+        self.player.release()
+        cv2.destroyAllWindows()
+        self.terminate()
+
+    def pause_stream(self):
+        self.gg = False
+
+
 
 
